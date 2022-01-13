67ab86b08e593a74c6f9d755af3c8d3aa4391b12
fox5500 <97639654+fox5500@users.noreply.github.com>
2022-01-13 16:59:46 -0500

    Update board-representation-att1.py

--- board-representation-att1.py
+++ board-representation-att1.py
@@ -1,2 +1,4 @@
 ## attempting to define a board enviroment that serves as backend for chess ai##
+## going to use the "square centric" environment The square centric representation implements the inverse association - is a square empty or is it occupied by a particular piece? The most popular square centric representations, mailbox or it's 0x88-enhancements - are basically arrays of direct piece-codes including the empty square and probably out of board codes. Hybrid solutions may further refer piece-list entries.##
+
 
